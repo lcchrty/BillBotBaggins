d7073367232f17f2ce85d1fa23137573a1a608eb
Chandler <lcchrty@gmail.com>
2023-09-08 14:12:45 -0700

    trying to establish functionality with grpc pub/sub api

--- app/api/redis/route.ts
+++ app/api/redis/route.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 // import client object to interact with redis and DB connection function from our redis.ts file
 import { client, redisConnect } from "../../../lib/redis";
 
+import { invoiceData, rawInvoiceData } from "@/app/types";
+
 /* 
  * GET request for retrieving invoice data from redis cache
 */
@@ -10,30 +12,22 @@ export const GET = async (req: NextRequest): Promise<NextResponse | undefined> =
 	try {
 		await redisConnect() //open redis connection on hot reload
 
-		// retrieve caches invoice reqords from redisDB
+		/**
+		 * retrieve caches invoice records from redisDB
+		 * need to update to account for new storage key-value pair where organziation anme is key and invoice data is value with each invoice store as an object in the array
+		 */
+
 		const cachedInvoicesParse = await client.get("newest invoices")
 
 		// type check for if what returns from client.get is a string to account for potential null
 		if (typeof cachedInvoicesParse === 'string') {
 			// parse string
 			const parsed = JSON.parse(cachedInvoicesParse);
-			// map to new object sorted  for ease pof data access
-			const cachedInvoices = parsed.map((element:
-				{
-					node: {
-						Id: string,
-						Invoice__c: { value: string },
-						Invoice_Sent_Date__c: { value: string },
-						npe01__Payment_Amount__c: { value: number },
-						Opportunity_Account_Name__c: { value: string },
-						Opportunity_18_Digit_ID__c: { value: string },
-						npe01__Payment_Method__c: { value: string },
-						npe01__Payment_Date__c: { value: string }
-					}
-				}) => {
+			// map to new object sorted for ease of data access
+			const cachedInvoices = parsed.map((element: rawInvoiceData): invoiceData => {
 				// destructure assignment for node object
 				const { Id, Invoice__c, Invoice_Sent_Date__c, npe01__Payment_Amount__c, Opportunity_Account_Name__c, Opportunity_18_Digit_ID__c, npe01__Payment_Method__c, npe01__Payment_Date__c } = element.node
-				// return sotrted object
+				// return sorted object
 				return ({
 					unique_invoice_id: Id,
 					invoice_number: Invoice__c.value,
--- app/types.ts
+++ app/types.ts
@@ -1,9 +1,45 @@
 
-// declaring an interface to reassign AxiosResponse implicit "any" return to acess array properties
+// declaring an ype to reassign AxiosResponse implicit "any" return to acess array properties
 
 export type ServerResponse = {
 	data: ServerData | null;
 }
 type ServerData = {
 	node: string[];
+}
+
+
+/**
+ * type reference for object received from salesforce and stored in redis cache
+ * see below, will also need to be updated
+ */
+
+export type rawInvoiceData = {
+	node: {
+		Id: string,
+		Invoice__c: { value: string },
+		Invoice_Sent_Date__c: { value: string },
+		npe01__Payment_Amount__c: { value: number },
+		Opportunity_Account_Name__c: { value: string },
+		Opportunity_18_Digit_ID__c: { value: string },
+		npe01__Payment_Method__c: { value: string },
+		npe01__Payment_Date__c: { value: string }
+	}
+}
+
+
+/**
+ * type reference for object retrieved and mapped from redis cache
+ * should update to store this in redis on salesforce call instead of retrieving and mapping 
+ */
+
+export type invoiceData = {
+	unique_invoice_id: string,
+	invoice_number: string,
+	invoice_amount: number,
+	invoice_sent_date: string,
+	invoice_paid_date: string,
+	payment_method: string,
+	opportunity_unique_id: string,
+	account_name: string
 }
